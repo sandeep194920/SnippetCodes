1bd9ba4443ae7ca4d5c11a25d077be5f6e6d0334
sandeep194920 <sandeepamaranath@gmail.com>
2022-01-16 14:43:14 -0500

    first-commit - installed formik hook

---
+++ src/Components/YoutubeForm.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import { useFormik } from 'formik'
+
+function YoutubeForm() {
+
+    const formik = useFormik({
+        initialValues: {
+            name: '',
+            email: '',
+            channel: ''
+        }
+    })
+    return (
+        <div>
+            <form>
+                <label htmlFor="name">Name</label>
+                <input type="text" id="name" name='name' />
+
+                <label htmlFor="email">E-mail</label>
+                <input type="email" id="email" name='email' />
+
+                <label htmlFor="channel">Channel</label>
+                <input type="text" id="channel" name='channel' />
+
+                <button>Submit</button>
+            </form>
+        </div>
+    )
+}
+
+export default YoutubeForm
